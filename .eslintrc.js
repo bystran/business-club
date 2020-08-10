c558d985fe25e49b6bac42155a27ec786dd83baa
Adam Bystran <bystran.adam.21@gmail.com>
2020-08-10 14:05:52 +0200

    Turns of line-break eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': [0, {}],
+    'linebreak-style': ['off', 'windows'],
   },
 };
